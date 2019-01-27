4f8db5d93d16d1ce7e0fcf4cf2707df73480beab
Itay Niv <itayniv@gmail.com>
2019-01-27 14:32:30 -0500

    init commit

---
+++ Masking-portal-ARkit/Assets/fix_position.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fix_position : MonoBehaviour {
+
+    public GameObject boxPosition;
+    private Vector3 CurrPosition;
+    private GameObject generatePlanesObject;
+
+
+	// Use this for initialization
+	void Start () {
+
+        generatePlanesObject = GameObject.FindWithTag("debugPlanes");
+
+        Debug.Log(boxPosition.transform.position + "box Position");
+        CurrPosition = boxPosition.transform.position;
+        Debug.Log(CurrPosition + "curr Position");
+
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if ( CurrPosition != boxPosition.transform.position){
+
+            generatePlanesObject.SetActive(false);
+            Destroy(generatePlanesObject, 0.5f);
+            
+        }
+		
+	}
+}
---
+++ Masking-portal-ARkit/Assets/moving_script.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class moving_script : MonoBehaviour {
+
+
+    public GameObject playanimation;
+    float timeLeft = 10.0f;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (timeLeft > 0){
+            
+            timeLeft -= Time.deltaTime;
+            if (timeLeft < 0)
+            {
+
+                playanimation =  GameObject.FindGameObjectWithTag("playAnimation");
+                playanimation.SetActive(false);
+                Destroy(playanimation, 0.2f);
+
+            }
+            
+        }
+       
+		
+	}
+
+
+
+  
+
+
+
+}
